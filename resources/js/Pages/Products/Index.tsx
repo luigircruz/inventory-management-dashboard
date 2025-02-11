0c5ae1579c8d2233dbf399538fe9e1f1efa09f47
Luigi Cruz <luigicruz.dev@gmail.com>
2025-02-11 18:41:46 +0800

    component: Create Product list view

---
+++ resources/js/Pages/Products/Index.tsx
@@ -0,0 +1,45 @@
+import InputError from '@/Components/InputError';
+import InputLabel from '@/Components/InputLabel';
+import PrimaryButton from '@/Components/PrimaryButton';
+import TextInput from '@/Components/TextInput';
+import Authenticated from '@/Layouts/AuthenticatedLayout';
+import { PageProps } from '@/types';
+import { Head, useForm } from '@inertiajs/react';
+
+export default function Index({ auth }: PageProps) {
+    const { data, setData, post, processing, reset, errors } = useForm({
+        name: '',
+    });
+
+    const submit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        console.log({ auth });
+        post(route('products.store'), { onSuccess: () => reset() });
+    };
+
+    return (
+        <Authenticated>
+            <Head title="Products" />
+
+            <div className="mx-auto max-w-2xl p-4 sm:p-6 lg:p-8">
+                <form onSubmit={submit}>
+                    <InputLabel htmlFor="name" value="Product Name" />
+                    <TextInput
+                        id="name"
+                        name="name"
+                        value={data.name}
+                        className="mt-1 block w-full"
+                        autoComplete="name"
+                        isFocused={true}
+                        onChange={(e) => setData('name', e.target.value)}
+                        required
+                    />
+                    <InputError message={errors.name} className="mt-2" />
+                    <PrimaryButton className="mt-4" disabled={processing}>
+                        Save
+                    </PrimaryButton>
+                </form>
+            </div>
+        </Authenticated>
+    );
+}
