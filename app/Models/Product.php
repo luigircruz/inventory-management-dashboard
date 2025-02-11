6bf3eb3d32809c33c7c5a64da2ad36e991861382
Luigi Cruz <luigicruz.dev@gmail.com>
2025-02-11 19:22:49 +0800

    models: Update Product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,11 +2,22 @@
 
 namespace App\Models;
 
+use App\Events\ProductCreated;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Product extends Model
 {
     protected $fillable = [
         'name',
     ];
+
+    protected $dispatchesEvents = [
+        'created' => ProductCreated::class,
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
