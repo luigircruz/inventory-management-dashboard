aca25469a4bf1aa28534e5e5929049a92958931e
Luigi Cruz <luigicruz.dev@gmail.com>
2025-02-12 19:03:45 +0800

    chores: Create and add configureCommands to AppServiceProvider's boot method

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 
@@ -22,6 +23,17 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Vite::prefetch(concurrency: 3);
+        $this->configureCommands();
         Model::unguard();
+    }
+
+    /**
+     * Configure the application's commands.
+     */
+    public function configureCommands(): void
+    {
+        DB::prohibitDestructiveCommands(
+            $this->app->environment('production')
+        );
     }
 }
