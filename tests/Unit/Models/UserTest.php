9d58655413685b2fa91b4e0382054ed90d4cbb67
Luigi Cruz <luigicruz.dev@gmail.com>
2025-02-13 17:27:36 +0800

    tests: Create User to array test

---
+++ tests/Unit/Models/UserTest.php
@@ -0,0 +1,16 @@
+<?php
+
+use App\Models\User;
+
+test('to array', function () {
+    $user = User::factory()->create()->fresh();
+
+    expect(array_keys($user->toArray()))->toBe([
+        'id',
+        'name',
+        'email',
+        'email_verified_at',
+        'created_at',
+        'updated_at',
+    ]);
+});
