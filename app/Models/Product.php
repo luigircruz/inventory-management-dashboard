2d8815a7f81e5042f2c537181e6126743af456b1
Luigi Cruz <luigicruz.dev@gmail.com>
2025-02-13 17:26:26 +0800

    model: Add HasFactory trait on product model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,11 +3,15 @@
 namespace App\Models;
 
 use App\Events\ProductCreated;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Product extends Model
 {
+    /** @use HasFactory<\Database\Factories\UserFactory> */
+    use HasFactory;
+
     protected $dispatchesEvents = [
         'created' => ProductCreated::class,
     ];
