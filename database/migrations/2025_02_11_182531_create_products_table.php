6f6d2e7f3aab9621d47938477140201bab483267
Luigi Cruz <luigicruz.dev@gmail.com>
2025-02-13 19:00:34 +0800

    models: Update products model

--- database/migrations/2025_02_11_182531_create_products_table.php
+++ database/migrations/2025_02_11_182531_create_products_table.php
@@ -14,8 +14,14 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->cascadeOnDelete();
-            $table->string('name');
+            $table->string('name')->unique();
             $table->string('status');
+            $table->text('description')->nullable();
+            $table->decimal('price', 15, 2);
+            $table->string('aisle')->nullable();
+            $table->integer('stock');
+            $table->string('image')->nullable();
+            $table->timestamp('published_at')->nullable();
             $table->timestamps();
         });
     }
