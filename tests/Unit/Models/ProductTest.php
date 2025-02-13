e1f7da51a98b6ae3d1fd59726ef42a9ee65004c1
Luigi Cruz <luigicruz.dev@gmail.com>
2025-02-13 17:27:46 +0800

    tests: Create product to array test

---
+++ tests/Unit/Models/ProductTest.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Models\Product;
+use App\Models\User;
+
+test('to array', function () {
+    $user = User::factory()->create()->fresh();
+    $product = Product::factory()->for($user)->create()->fresh();
+
+    expect(array_keys($product->toArray()))->toBe([
+        'id',
+        'user_id',
+        'name',
+        'created_at',
+        'updated_at',
+    ]);
+});
